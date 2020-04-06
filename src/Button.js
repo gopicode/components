14090ca8d2803c540c3c85d55385da396ba0437a
Gopinath Kesavan <gopinath.k@myntra.com>
2020-04-06 17:19:30 +0530

    removed files that are moved to other repository

--- src/Button.js
+++
@@ -1,9 +0,0 @@
-import {h} from './h.js'
-import './Button.css';
-
-// Refer: http://exploringjs.com/es6/ch_destructuring.html for ...rest syntax
-export function Button(propsAll) {
-	const {children, ...props} = propsAll;
-	props.className = 'btn';
-	return h('button', props, children);
-}
